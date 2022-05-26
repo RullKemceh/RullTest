79e9b97dff0c283f0e2927b8a21333d0888e7d23
RullKemceh <103581067+RullKemceh@users.noreply.github.com>
2022-05-27 04:34:20 +0700

    Update config.js

--- config.js
+++ config.js
@@ -22,16 +22,16 @@ global.APIKeys = {
 }
 
 // Other
-global.owner = ['6281285866502', '6281398003097']
-global.botname = 'BAYUBOT-MD'
-global.ownername = 'BAYU OFC'
-global.packname = 'BAYUBOT-MD'
-global.author = 'CREATED BY BAYU OFC'
+global.owner = ['6285859047172', '6285859047172']
+global.botname = 'RULL-MD'
+global.ownername = 'RULL'
+global.packname = 'RULLBOT-MD'
+global.author = 'CREATED BY RULL'
 global.sessionName = 'session'
 global.prefa = ['#','!','.','🐦','🐤','🗿']
 global.sp = '⭔'
 global.mess = {
-    success: 'Done !! Subscribe BAYU OFC',
+    success: 'Done !!',
     admin: 'Fitur Khusus Admin Group!',
     botAdmin: 'Bot Harus Menjadi Admin Terlebih Dahulu!',
     owner: 'Fitur Khusus Owner Bot',
